7cb405cafc6f20ea5eaa9277165134af35c4ab2f
yuanyyb@hotmail.com <yuanyyb@hotmail.com>
2017-06-29 12:40:48 +0800

    评分修改后未完善

--- score/pingfen.js
+++ score/pingfen.js
@@ -1,54 +1,70 @@
-window.onload = function(){
-        var aLi = document.getElementsByTagName('li');
-        var oDiv = document.getElementById('content');
-        var oSpan = oDiv.getElementsByTagName('span')[0];
-        for(var i=0;i<aLi.length;i++){
-            aLi[i].index = i;
+window.onload = function() {
+    var aLi = document.getElementsByTagName('li');
+    var oDiv = document.getElementById('content');
+    var oSpan = oDiv.getElementsByTagName('span')[0];
+    for (var i = 0; i < aLi.length; i++) {
+        aLi[i].index = i;
 
-            aLi[i].onclick = function(){
-                for(var j=0;j<aLi.length;j++){
-                    aLi[j].checked='';
-                }
-                for(var i=0;i<=this.index;i++){
-                    aLi[i].className='active';
+        aLi[i].onclick = function() {
+            for (var j = 0; j < aLi.length; j++) {
+                aLi[j].checked = '';
+            }
+            for (var i = 0; i <= this.index; i++) {
+                if (this.index < 2) {
+                    aLi[i].className = 'chaactive';
+                    aLi[i].checked = '1';
+                } else {
+                    aLi[i].className = 'active';
                     aLi[i].checked = '1';
                 }
-            };
 
-            aLi[i].onmouseover = function(){
-                for(var j=0;j<aLi.length;j++){
-                    aLi[j].className='';
+            }
+        };
+
+        aLi[i].onmouseover = function() {
+            for (var j = 0; j < aLi.length; j++) {
+                aLi[j].className = '';
+            }
+            for (var i = 0; i <= this.index; i++) {
+                aLi[i].className = 'active';
+            }
+            if (this.index < 2) {
+                for (var i = 0; i <= this.index; i++) {
+                    aLi[i].className = 'chaactive';
                 }
-                for(var i=0;i<=this.index;i++){
-                    aLi[i].className='active';
+            }
+            showLevel(this.index);
+        };
+        aLi[i].onmouseout = function() {
+            //this.className='';
+            for (var i = 0; i < aLi.length; i++) {
+                if (aLi[i].checked != '1') {
+                    aLi[i].className = '';
+                } else {
+                    //aLi[i].className="active";
+                    showLevel(i);
                 }
-                showLevel(this.index);
-            };
-            aLi[i].onmouseout = function(){
-                //this.className='';
-                for(var i=0;i<aLi.length;i++){
-                    if(aLi[i].checked!='1'){
-                        aLi[i].className='';
-                    }else{
-                        aLi[i].className="active";
-                        showLevel(i);
-                    }
-                }
+            }
 
 
-            };
-        }
-        function showLevel(num){
-        if(num<2){
-                    oSpan.innerHTML = '差';
-                }else if(num==2){
-                    oSpan.innerHTML='中';
-                }else if(num==3){
-                    oSpan.innerHTML='良';
-                }else if(num==4){
-                    oSpan.innerHTML='优';
-                }
+        };
     }
-    };
 
-    
+    function showLevel(num) {
+        if (num < 2) {
+            oSpan.innerHTML = '差';
+            aLi[num].className = 'chaactive';
+        } else if (num == 2) {
+            oSpan.innerHTML = '中';
+            aLi[0].className = 'active';
+            aLi[1].className = 'active';
+        } else if (num == 3) {
+            oSpan.innerHTML = '良';
+        } else if (num == 4) {
+            oSpan.innerHTML = '优';
+        }
+
+
+
+    }
+};
