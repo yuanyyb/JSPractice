006fe28fd4fde561f3bd299d08e517c3e9a26475
yuanyyb@hotmail.com <yuanyyb@hotmail.com>
2017-07-06 11:36:01 +0800

    修改再次点击无法回复原颜色问题

--- InterlaceColorChange/rows.js
+++ InterlaceColorChange/rows.js
@@ -1,32 +1,34 @@
-window.onload = function(){
+window.onload = function() {
     var oUl = document.getElementById('rows');
-    var colorArray = ['red','yellow','green','white','black'];
-    for(var i=0;i<50;i++){
-        oUl.innerHTML += '<li style="background:'+colorArray[i%colorArray.length]+'"></li>';
+    var colorArray = ['red', 'yellow', 'green', 'white', 'black'];
+    for (var i = 0; i < 50; i++) {
+        oUl.innerHTML += '<li style="background:' + colorArray[i % colorArray.length] + '"></li>';
     }
     var aLi = oUl.getElementsByTagName('li');
-    for(var j=0;j<aLi.length;j++){
-        aLi[j].onmouseover = function(){
-            this.oldColor = this.style.backgroundColor;
+    for (var j = 0; j < aLi.length; j++) {
+        aLi[j].onmouseover = function() {
+            if (!this.oldColor) {
+                this.oldColor = this.style.backgroundColor;
+            }
             this.style.backgroundColor = 'pink';
         };
-        aLi[j].onmouseout = function(){
-            
-            if(this.clickColor&&this.clickColor!=''){
+        aLi[j].onmouseout = function() {
+
+            if (this.clickColor && this.clickColor != '') {
                 this.style.backgroundColor = 'blue';
-            }else{
+            } else {
                 this.style.backgroundColor = this.oldColor;
             }
         };
-        aLi[j].onclick = function(){
-            if(this.clickColor&&this.clickColor!=''){
-                this.clickColor='';
+        aLi[j].onclick = function() {
+            if (this.clickColor && this.clickColor != '') {
+                this.clickColor = '';
                 this.style.backgroundColor = this.oldColor;
-            }else{
-                this.clickColor='blue';
+            } else {
+                this.clickColor = 'blue';
                 this.style.backgroundColor = 'blue';
             }
-            
+
         };
     }
 
